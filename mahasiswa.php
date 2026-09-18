819f80bbd4aa8edb9cf9bafca6c16371d206bdc1
ade putra <2511500078@atmaluhur.ac.id>
2026-09-18 18:08:28 +0700

    initial commit

---
+++ mahasiswa.php
@@ -0,0 +1,15 @@
+
+
+{
+  "status": "success",
+  "message": "data mahasiswa berhasil diambil",
+  "data": {
+    "id":1,
+    "nim":"2511500078",
+    "nama":"ade putra",
+    "email":"ade@gmail.com",
+    "jurusan": "teknik informatika",
+    "semester": 5
+    "ipk": 3,75
+  }
+}
